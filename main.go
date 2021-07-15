3fc964ef419bebe682db02b7dd6d6292638a4ba1
asd asd <b.muhlhausera@gmail.com>
2021-07-15 12:18:06 -0400

    Created principal funcions mock

--- main.go
+++ main.go
@@ -13,6 +13,48 @@ func main() {
 
 	req, err := http.NewRequest("GET", "https://api.hackerone.com/v1/hackers/me/reports", nil)
 	req.Header = headers
+	req.SetBasicAuth("xpl0ited1", "TOKEN")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	log.Println(string(body))
+}
+
+func createReport() {
+	headers := map[string][]string{
+		"Accept": []string{"application/json"},
+	}
+
+	req, err := http.NewRequest("GET", "https://api.hackerone.com/v1/hackers/me/reports", nil)
+	req.Header = headers
+	req.SetBasicAuth("username", "TOKEN")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	log.Println(string(body))
+}
+
+func getProgramWeakness(programHandle string) {
+	headers := map[string][]string{
+		"Accept": []string{"application/json"},
+	}
+
+	req, err := http.NewRequest("GET", "https://api.hackerone.com/v1/hackers/programs/{handle}/weaknesses", nil)
+	req.Header = headers
 	req.SetBasicAuth("username", "TOKEN")
 
 	client := &http.Client{}
